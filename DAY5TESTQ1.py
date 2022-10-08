b39fcea1c058017ebd2351b73f44c29b6d8efca8
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-08 08:50:55 +0530

    Add files via upload

---
+++ DAY5TESTQ1.py
@@ -0,0 +1,7 @@
+string=input("enter string:")
+p=""
+for char in string:
+    if char not in p:
+        p=p+char
+print(p)
+k=list("string")
---
+++ OUTPUTDAY5TESTQ1.py
@@ -0,0 +1,31 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:HELLO WORLD
+HELO WRD
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:HEY YOU
+HEY OU
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:TAKE CARE
+TAKE CR
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:GOOD EVENING
+GOD EVNI
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:RED ROSE
+RED OS
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:123456S
+123456S
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+enter string:FLOWER
+FLOWER
+>>> 
