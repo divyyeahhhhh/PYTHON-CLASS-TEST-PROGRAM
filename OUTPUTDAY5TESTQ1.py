311fe3d25baecb1601500722dc7e6060fee92d7b
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-08 09:10:13 +0530

    Add files via upload

---
+++ OUTPUTDAY5TESTQ1.py
@@ -0,0 +1,28 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+Enter the string: HELLO WORLD
+Enter a character to be deleted: L
+String after character removed:  HEO WORD
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+Enter the string: TAKE CARE
+Enter a character to be deleted: E
+String after character removed:  TAK CAR
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+Enter the string: GOOD EVENING
+Enter a character to be deleted: OE
+String after character removed:  GOOD EVENING
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+Enter the string: RED ROSE
+Enter a character to be deleted: R
+String after character removed:  ED OSE
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/DAY5TESTQ1.py 
+Enter the string: HEY YOU
+Enter a character to be deleted: Y
+String after character removed:  HE OU
+>>> 
